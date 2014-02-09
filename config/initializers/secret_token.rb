3870d822f124377e421c9370a259377af13fd28c
Frederik Bruneel <frederik@xuntar.com>
2014-02-09 12:40:08 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CasaAzahar::Application.config.secret_key_base = '3bbc610f163306fdedbea18c9926ce329125400689f6618c1641b99adb8c7f8db8409cbae2efa7b48d897f9289b78b6f47aef8ddc9487e301a8677e45b467102'
